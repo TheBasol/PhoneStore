00a055dc97ae8b3be86d813a0039d64695c44566
TheBasol <87411760+TheBasol@users.noreply.github.com>
2022-01-26 18:35:46 -0600

    First

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// inicializamos AOS libreria
+AOS.init();
+
+let menu = document.getElementById('menu');
+let menuBar = document.getElementById('menu-bar');
+
+menuBar.addEventListener("click", () =>{
+    menu.classList.toggle("menu-toggle");
+})
